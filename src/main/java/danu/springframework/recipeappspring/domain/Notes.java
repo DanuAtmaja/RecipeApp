3cf83e9473e941f83c458ccf2fd616f6f1bdb551
Danu Atmaja <danuatmaja@gmail.com>
2021-03-31 08:06:04 +0800

    initialize app generated domain and controllers with templates index

---
+++ src/main/java/danu/springframework/recipeappspring/domain/Notes.java
@@ -0,0 +1,42 @@
+package danu.springframework.recipeappspring.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+//    Lob Annotation used to save more than 255 character sometimes give aliases as BLOBS
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
